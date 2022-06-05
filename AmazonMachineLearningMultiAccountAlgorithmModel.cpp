03c3273a5c6277e4b038f0b196a886019648b82d
alfonsoDeveloperTop <106808155+alfonsoDeveloperTop@users.noreply.github.com>
2022-06-05 04:50:31 -0700

    Add files via upload
    My  description   about  this  project  amazon  machine learning  multiaccount  C++ is  based  on  codes   algorithms  from  the  amazon  machine learning   websites  showed  in this  zip  folder  attachments  to  be discussed  and  shared

---
+++ AmazonMachineLearningMultiAccountAlgorithmModel.cpp
@@ -0,0 +1,48 @@
+#include<algorithm>
+#include <aws/s3/S3Client.h>
+#include<aws/s3/model/PutObjectRequest.h>
+#include<aws/core/Aws.h>
+#include<aws/core/utils/memory/stl/AWSStringStream.h>
+#include<string>
+
+using namespace Aws::S3;
+using namespace Aws::S3::Model;
+
+static const char* KEY = "s3_cpp_sample_key";
+static const char* BUCKET = "s3-cpp-sample-bucket";
+// Create template  machine learning  algorithm  search for Sagemaker-role multiaccount parameters  function
+
+void CreateTemplateMachineLearningAWS_Algorithms(_ALGORITHM_) {
+	__cpp_variable_templates _ALGORITHM_ SearchFor(typename NAMEPrefixSagemaker_role);
+	__cpp_variable_templates _ALGORITHM_ SearchFor(typename  NAMESagemaker_role_suffix);
+}
+
+// Create configuring data science account organizations AWS arguments 
+bool ConfiguringDataScienceAccountOrganizationsAWS(string __type_info_root_node) {
+
+	_type_info_root_node = "multi-account-deployment t(OU)";
+
+
+
+	return  true;
+
+}
+
+int main() {
+
+	Aws::SDKOptions override options;
+	Aws::InitAPI(options) {
+
+		S3Client  client;
+
+		// First put on object into S3
+		PutObjectRequest putObjectRequest;
+		__cpp_attributes putObjectRequest.WithKey(KEY);
+		__cpp_attributes.WithBucket(BUCKET);
+
+	}
+
+	
+
+}
+
